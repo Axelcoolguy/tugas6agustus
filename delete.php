353939781d988578002d3d3353f1ba31a38aa156
EncryptCodeX <69043865+Axelcoolguy@users.noreply.github.com>
2026-09-03 11:20:39 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -8,13 +8,11 @@ if(!isset($_SESSION['username'])){
 include 'koneksi.php';
 $id = $_GET['id'];
 
-// Ambil info file foto lalu hapus dari folder uploads
 $data = mysqli_fetch_array(mysqli_query($koneksi, "SELECT foto FROM barang_bukti WHERE id='$id'"));
 if (file_exists("uploads/" . $data['foto'])) {
     unlink("uploads/" . $data['foto']);
 }
 
-// Hapus record dari database
 mysqli_query($koneksi, "DELETE FROM barang_bukti WHERE id='$id'");
 header("Location: index.php");
 ?>
