f730fa9ffefaf6d86feb0ebdbbbd86d7d68b7b62
EncryptCodeX <69043865+Axelcoolguy@users.noreply.github.com>
2026-09-03 11:04:47 +0700

    Create login.php

---
+++ login.php
@@ -0,0 +1,53 @@
+<?php
+session_start();
+include 'koneksi.php';
+
+// Jika sudah login, langsung lempar ke index
+if (isset($_SESSION['username'])) {
+    header("Location: index.php");
+    exit();
+}
+
+$error = '';
+
+if (isset($_POST['login'])) {
+    $username = mysqli_real_escape_string($koneksi, $_POST['username']);
+    $password = $_POST['password'];
+
+    // Query cek user di database yang sama
+    $query = mysqli_query($koneksi, "SELECT * FROM users WHERE username='$username' AND password='$password'");
+    
+    if (mysqli_num_rows($query) > 0) {
+        $data = mysqli_fetch_assoc($query);
+        $_SESSION['username'] = $data['username'];
+        header("Location: index.php");
+        exit();
+    } else {
+        $error = "Username atau password salah!";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login - Database Barang Bukti</title>
+</head>
+<body>
+    <h2>Login Sistem Barang Bukti</h2>
+    
+    <?php if ($error != ''): ?>
+        <p style="color: red;"><?= $error; ?></p>
+    <?php endif; ?>
+
+    <form action="" method="POST">
+        <label>Username:</label><br>
+        <input type="text" name="username" required><br><br>
+
+        <label>Password:</label><br>
+        <input type="password" name="password" required><br><br>
+
+        <button type="submit" name="login">Login</button>
+    </form>
+</body>
+</html>
